9e37df9fbed77f5875b91984ba0be20c97ad61dc
Kerem Dlkmn <15542014@firat.edu.tr>
2019-03-25 19:29:36 +0300

    Create BasicStatistic.py

---
+++ BasicStatistic.py
@@ -0,0 +1,34 @@
+from math import sqrt
+"""
+    Basic Statistic Module
+"""
+
+def avarageList(AnyList):
+    """
+    This method allows us to find the average of a list
+    :param AnyList: Any numeric list
+    :return:
+    """
+    anyListLen = len(AnyList)
+    sumList    = 0
+
+    for i in range(anyListLen):
+        sumList = sumList + AnyList[i]
+
+    return (sumList / anyListLen)
+
+def standardDeviation(AnyList):
+    """
+    This method allows us to find the standard deviation of a list
+    :param AnyList: Any numeric list
+    :return:
+    """
+    anyListLen             = len(AnyList)          #Listemin Boyutu     N
+    responseAvarageList    = avarageList(AnyList) #Listemin Ortalaması X'
+    sumList                = 0
+
+    for i in range(anyListLen):
+        sumList = sumList + pow((AnyList[i] - responseAvarageList),2)
+
+    sqrtList = ((sumList)/(anyListLen - 1))
+    return sqrt(sqrtList)
